730d55a70e7dcde6e353a86d2ccba3b7f30c7b47
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-08-12 21:15:03 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const reviews = [
+  {
+    id: 1,
+    name: "ANMS・BEAST (5390931838)",
+    job: "Sniper",
+    image: "./images/1.jpg",
+    image2: "./images/5.jpg",
+    ig:"https://www.instagram.com/vivek__30/",
+    text: "Sniper is one of the most underrated roles in a squad. BGMI is not all about assaulting and using automatic guns. Most players and squads do not realize the importance of a sniper. The sniper is capable of picking up knocks and kills at a distance. This role takes a lot of patience and precision, and not a lot of players can execute the role very well. ",
+  },
+  {
+    id: 2,
+    name: "Zulmì (571978418)",
+    job: "The Fragger",
+    image: "./images/2.jpg",
+    image2: "./images/4.jpg",
+    ig:"https://www.instagram.com/r0hhhit31/",
+    text: "I am the fragger or assaulter of my Team  and i am responsible for the attack on the enemies. I needs to be at the forefront of the attack. The main responsibility of me is to move towards the enemy and create an opening for the other players of the team.",
+  },
+  {
+    id: 3,
+    name: "DC쓰WeapoN (572202449)",
+    job: "The Healer",
+    image: "./images/3.jpg",
+    image2: "./images/6.jpg",
+    ig:"https://www.instagram.com/_.prashant_sharma_/",
+    text: "I always needs to be in cover and support my team. My main task is to have a good stock of supplies like health kits, medkits, painkillers, etc. Also one of the responsibilities is to revive the knocked out players",
+  },
+];
+
+export default reviews;
